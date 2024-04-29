ddb187a4c7809f1b25a0cf05c00237466eb16dd3
ItaloCobains <italobrandao.contato@gmail.com>
2024-04-29 14:30:38 -0300

    Complete merge two sorted lists

---
+++ merge-two-sorted-lists/index.rb
@@ -0,0 +1,44 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} list1
+# @param {ListNode} list2
+# @return {ListNode}
+def merge_two_lists(list1, list2)
+  return list1 if list2.nil?
+  return list2 if list1.nil?
+
+  if list1.val < list2.val
+    list1.next = merge_two_lists(list1.next, list2)
+    return list1
+  else
+    list2.next = merge_two_lists(list1, list2.next)
+    return list2
+  end
+end
+
+class ListNode
+  attr_accessor :val, :next
+  def initialize(val = 0, _next = nil)
+    @val = val
+    @next = _next
+  end
+end
+
+def create_list(arr)
+  head = ListNode.new
+  current = head
+  arr.each do |val|
+    current.next = ListNode.new(val)
+    current = current.next
+  end
+  head.next
+end
+
+p merge_two_lists(create_list([1, 2, 4]), create_list([1, 3, 4]))
+# => [1, 1, 2, 3, 4, 4]
